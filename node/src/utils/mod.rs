695abc4dfa1e05c16fbaff0dd2030debeca88323
David Kaplan <davkaplan@gmail.com>
2026-02-08 16:22:17 +0200

    refactor: remove dead connector utilities and unused windows package module

--- node/src/utils/mod.rs
+++ node/src/utils/mod.rs
@@ -4,15 +4,7 @@ pub mod semantic_parser;
 mod system;
 #[cfg(windows)]
 mod ui_automation;
-#[cfg(windows)]
-mod windows_packages;
 
 #[allow(unused_imports)]
 pub use process::*;
 pub use system::*;
-#[cfg(windows)]
-#[allow(unused_imports)]
-pub use ui_automation::*;
-#[cfg(windows)]
-#[allow(unused_imports)]
-pub use windows_packages::*;
